bd1a532a1993057beeca854bea91564d0e201558
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-06-16 20:34:11 +0530

    Update ToDo.java

--- app-tier/src/main/java/com/example/todo/entity/ToDo.java
+++ app-tier/src/main/java/com/example/todo/entity/ToDo.java
@@ -1,9 +1,13 @@
 package com.example.todo.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "todos")
+@Data
 public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,6 +17,4 @@ public class ToDo {
     private String description;
     private LocalDate dueDate;
     private boolean completed;
-
-    // Getters and setters
 }
