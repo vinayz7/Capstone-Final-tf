b3bacf19d97f210bb79320b7043d7cb9ee4c182e
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-06-16 20:34:46 +0530

    Update ToDoDto.java

--- app-tier/src/main/java/com/example/todo/dto/ToDoDto.java
+++ app-tier/src/main/java/com/example/todo/dto/ToDoDto.java
@@ -1,13 +1,14 @@
 package com.example.todo.dto;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class ToDoDto {
     private Long id;
     private String title;
     private String description;
     private LocalDate dueDate;
     private boolean completed;
-
-    // Getters and setters
 }
